49c6035c625682af4bfbc727050a66c6f3c074ca
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-13 02:25:35 -0800

    Import then render header component within the parent

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/Header";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
       </div>
     )
   }
