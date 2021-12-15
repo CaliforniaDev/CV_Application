0f9036c35496022affdd0bb5d8d935c43f3e67fe
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-14 20:13:53 -0800

    Add component Personal.js for general information

---
+++ cv-project/src/components/Personal.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Personal extends React.Component {
+    render() {
+        return (
+            <div className="styles.topSection">
+                <label>First Name</label>
+                <input type='text' name="firstName" /> 
+                <label>Last Name</label>
+                <input type='text' name='lastName' />
+                <label>Email</label>
+                <input type='text' name='email' />
+                <label>Phone Number</label>
+                <input type='text' name='phoneNumber' />
+
+            </div>
+        )
+    }
+}
+
+export default Personal;
