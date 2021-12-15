bb90cf01b39d3b3a545fb0198019dad3581c7950
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-14 20:23:26 -0800

    Import styles module

--- cv-project/src/components/Personal.js
+++ cv-project/src/components/Personal.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "../styles/Personal.module.scss";
 
 class Personal extends React.Component {
     render() {
         return (
-            <div className="styles.topSection">
+            <div className={styles.topSection}>
                 <label>First Name</label>
                 <input type='text' name="firstName" /> 
                 <label>Last Name</label>
