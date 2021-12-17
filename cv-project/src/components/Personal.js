b77226a9d5dc8e4bd7a227f0d78189fbdb6c812e
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-16 18:24:10 -0800

    Add header and Change div container to section tag

--- cv-project/src/components/Personal.js
+++ cv-project/src/components/Personal.js
@@ -4,17 +4,18 @@ import styles from "../styles/Personal.module.scss";
 class Personal extends React.Component {
     render() {
         return (
-            <div className={styles.topSection}>
-                <label>First Name</label>
-                <input type='text' name="firstName" /> 
-                <label>Last Name</label>
-                <input type='text' name='lastName' />
-                <label>Email</label>
-                <input type='text' name='email' />
-                <label>Phone Number</label>
-                <input type='text' name='phoneNumber' />
+                <section className={styles.topSection}>
+                    <h3>Personal Information</h3>
+                    <label>First Name</label>
+                    <input type='text' name="firstName" />
+                    <label>Last Name</label>
+                    <input type='text' name='lastName' />
+                    <label>Email</label>
+                    <input type='text' name='email' />
+                    <label>Phone Number</label>
+                    <input type='text' name='phoneNumber' />
 
-            </div>
+                </section>
         )
     }
 }
