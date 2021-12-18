9bea471938529374ef399a8f0b30f6584a4878ab
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-17 16:12:54 -0800

    Add event handler to each input with each value set

--- cv-project/src/components/Personal.js
+++ cv-project/src/components/Personal.js
@@ -6,22 +6,41 @@ class Personal extends React.Component {
         super();
     }
     render() {
-        const { handleChangeProp } = this.props;
+        const { personalInfo, handleChange } = this.props;
         return (
                 <section className={styles.topSection}>
                     <h3>Personal Information</h3>
+                    
                     <label>First Name</label>
-                    <input onChange={handleChangeProp} type='text' name="firstName" />
-                    <label>Last Name</label>
-                    <input onChange={handleChangeProp} type='text' name='lastName' />
-                    <label>Email</label>
-                    <input onChange={handleChangeProp} type='text' name='email' />
-                    <label>Phone Number</label>
-                    <input onChange={handleChangeProp} type='text' name='phoneNumber' />
+                    <input 
+                      onChange={handleChange} 
+                      value={personalInfo.firstName} 
+                      type='text' name="firstName" 
+                    />
 
+                    <label>Last Name</label>
+                    <input 
+                      onChange={handleChange} 
+                      value={personalInfo.lastName} 
+                      type='text' name='lastName' 
+                    />
+
+                    <label>Email</label>
+                    <input 
+                      onChange={handleChange}
+                      value={personalInfo.email}
+                      type='text' name='email' 
+                    />
+
+                    <label>Phone Number</label>
+                    <input 
+                      onChange={handleChange}
+                      value={personalInfo.phoneNumber} 
+                      type='text' name='phoneNumber'
+                    />
                 </section>
         )
     }
 }
-
+// FIX STATE AND RENDERING THE INFO FROM THE STATE;
 export default Personal;
