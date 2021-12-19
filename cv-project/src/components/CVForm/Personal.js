120b2b3b9cd3ab5f574e4661db11b3accbe2cf62
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-18 18:12:35 -0800

    Remove input labels and replace with placeholders

--- cv-project/src/components/CVForm/Personal.js
+++ cv-project/src/components/CVForm/Personal.js
@@ -2,45 +2,47 @@ import React from "react";
 import styles from "../../styles/Personal.module.scss";
 
 class Personal extends React.Component {
-    constructor(props) {
-        super();
-    }
-    render() {
-        const { personalInfo, handleChange } = this.props;
-        return (
-                <section className={styles.topSection}>
-                    <h3>Personal Information</h3>
-                    
-                    <label>First Name</label>
-                    <input 
-                      onChange={handleChange} 
-                      value={personalInfo.firstName} 
-                      type='text' name="firstName" 
-                    />
+  constructor(props) {
+    super();
+  }
+  render() {
+    const { personalInfo, handleChange } = this.props;
+    return (
+      <section className={styles.sectionContainer}>
+        <h3>Personal Information</h3>
+        <div className={styles.wrapper} >
+          <input
+            onChange={handleChange}
+            value={personalInfo.firstName}
+            type='text' name="firstName"
+            placeholder="First name"
+          />
 
-                    <label>Last Name</label>
-                    <input 
-                      onChange={handleChange} 
-                      value={personalInfo.lastName} 
-                      type='text' name='lastName' 
-                    />
+          <input
+            onChange={handleChange}
+            value={personalInfo.lastName}
+            type='text' name='lastName'
+            placeholder="Last name"
+          />
 
-                    <label>Email</label>
-                    <input 
-                      onChange={handleChange}
-                      value={personalInfo.email}
-                      type='text' name='email' 
-                    />
+          <input
+            onChange={handleChange}
+            value={personalInfo.email}
+            type='text' name='email'
+            placeholder="Email"
+          />
 
-                    <label>Phone Number</label>
-                    <input 
-                      onChange={handleChange}
-                      value={personalInfo.phoneNumber} 
-                      type='text' name='phoneNumber'
-                    />
-                </section>
-        )
-    }
+          <input
+            onChange={handleChange}
+            value={personalInfo.phoneNumber}
+            type='text' name='phoneNumber'
+            placeholder="Phone number"
+          />
+        </div>
+
+      </section>
+    )
+  }
 }
 // FIX STATE AND RENDERING THE INFO FROM THE STATE;
 export default Personal;
