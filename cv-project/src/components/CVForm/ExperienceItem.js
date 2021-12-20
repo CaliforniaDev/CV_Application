ffcd6b6a1e45f2ca855504fdcbd84a59c2b31e03
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-19 19:59:40 -0800

    Add experience item component

---
+++ cv-project/src/components/CVForm/ExperienceItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ExperienceItem extends React.Component {
+    constructor(props) {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                Experience Item.
+            </div>
+        )
+    }
+}
