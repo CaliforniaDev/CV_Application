9d167a93ee799c01252f5ca6cb06cbcde34ade20
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-20 17:25:16 -0800

    Add delete button

--- cv-project/src/components/CVForm/ExperienceItem.js
+++ cv-project/src/components/CVForm/ExperienceItem.js
@@ -1,45 +1,52 @@
 import styles from '../../styles/Experience.module.scss';
+import styledButton from '../../styles/Button.module.scss';
+import Button from '../Utils/Button';
 
-const ExperienceItem = ({ experienceItem, id, onChange}) => {
+const ExperienceItem = ({ experienceItem, id, onChange }) => {
 
-    return (
-        <div className={styles.wrapper}>
-            <input
-              value={experienceItem.position}
-              onChange={e => onChange(e, id)}
-              type='text' name='position'
-              placeholder='Position'
-            />
+  return (
+    <div className={styles.wrapper}>
+      <input
+        value={experienceItem.position}
+        onChange={e => onChange(e, id)}
+        type='text' name='position'
+        placeholder='Position'
+      />
 
-            <input
-              value={experienceItem.company}
-              onChange={e => onChange(e, id)}
-              type='text' name='company'
-              placeholder='Company'
-            />
+      <input
+        value={experienceItem.company}
+        onChange={e => onChange(e, id)}
+        type='text' name='company'
+        placeholder='Company'
+      />
 
-            <input
-              value={experienceItem.city}
-              onChange={e => onChange(e, id)}
-              type='text' name='city'
-              placeholder='City'
-            />
+      <input
+        value={experienceItem.city}
+        onChange={e => onChange(e, id)}
+        type='text' name='city'
+        placeholder='City'
+      />
 
-            <input
-              value={experienceItem.from}
-              onChange={e => onChange(e, id)}
-              type='text' name='from'
-              placeholder='From'
-            />
+      <input
+        value={experienceItem.from}
+        onChange={e => onChange(e, id)}
+        type='text' name='from'
+        placeholder='From'
+      />
 
-            <input
-              value={experienceItem.to}
-              onChange={e => onChange(e, id)}
-              type='text' name='to'
-              placeholder='To'
-            />
-        </div>
-    )
+      <input
+        value={experienceItem.to}
+        onChange={e => onChange(e, id)}
+        type='text' name='to'
+        placeholder='To'
+      />
+      <Button
+        classNameProp={styledButton.deleteButton}
+        onClick={(e) => { e.preventDefault() }}
+        name="Delete"
+      />
+    </div>
+  )
 }
 
 export default ExperienceItem;
