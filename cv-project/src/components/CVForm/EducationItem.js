c5ad557c47852041cb671f55878ab736476ecdb4
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-27 00:51:27 -0800

    Define onChange event handler to all input items

--- cv-project/src/components/CVForm/EducationItem.js
+++ cv-project/src/components/CVForm/EducationItem.js
@@ -1,34 +1,40 @@
 import styles from "../../styles/Education.module.scss";
-const EducationItem = () => {
+const EducationItem = ( onChangeEducation ) => {
     return (
         <div className={styles.wrapper}>
             <input 
               type="text" name="school"
+              onChange={onChangeEducation}
               placeholder="University Name" 
             />
 
             <input
               type="text" name="city"
+              onChange={onChangeEducation}
               placeholder="City"
             />
 
             <input
               type="text" name="degree"
+              onChange={onChangeEducation}
               placeholder="Degree" 
             />
 
             <input
               type="text" name="subject"
+              onChange={onChangeEducation}
               placeholder="Subject" 
             />
 
             <input
               type="text" name="from"
+              onChange={onChangeEducation}
               placeholder="From" 
             />
-            
+
             <input
               type="text" name="to"
+              onChange={onChangeEducation}
               placeholder="To" 
             />
         </div>
