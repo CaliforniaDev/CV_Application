c6df22acc8bc898081566be3c9b8e403a0dbde7a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-27 00:28:55 -0800

    Define className attribute

--- cv-project/src/components/CVForm/EducationItem.js
+++ cv-project/src/components/CVForm/EducationItem.js
@@ -1,7 +1,7 @@
 import styles from "../../styles/Education.module.scss";
 const EducationItem = () => {
     return (
-        <div>
+        <div className={styles.wrapper}>
             Education Item
         </div>
     )
