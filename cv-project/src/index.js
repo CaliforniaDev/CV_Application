5106f1605ebf3d68fdfa4a4ab86f7f7703051ca1
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-27 15:21:55 -0800

    Change style sheet to scss

--- cv-project/src/index.js
+++ cv-project/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'normalize.css';
-import './styles/App.css';
+import './styles/App.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
