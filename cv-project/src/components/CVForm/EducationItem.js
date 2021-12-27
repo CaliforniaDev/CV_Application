14ba3267f9064b1054af85457dd0373ceb496c0b
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-27 00:34:46 -0800

    Add all input fields to education component

--- cv-project/src/components/CVForm/EducationItem.js
+++ cv-project/src/components/CVForm/EducationItem.js
@@ -2,7 +2,35 @@ import styles from "../../styles/Education.module.scss";
 const EducationItem = () => {
     return (
         <div className={styles.wrapper}>
-            Education Item
+            <input 
+              type="text" name="school"
+              placeholder="University Name" 
+            />
+
+            <input
+              type="text" name="city"
+              placeholder="City"
+            />
+
+            <input
+              type="text" name="degree"
+              placeholder="Degree" 
+            />
+
+            <input
+              type="text" name="subject"
+              placeholder="Subject" 
+            />
+
+            <input
+              type="text" name="from"
+              placeholder="From" 
+            />
+            
+            <input
+              type="text" name="to"
+              placeholder="To" 
+            />
         </div>
     )
 }
