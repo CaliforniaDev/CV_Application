951db170d3b380835e5b70a85472b4e31666814a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-27 00:14:16 -0800

    Create education component

---
+++ cv-project/src/components/CVForm/Education.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Education extends React.Component {
+    constructor(props) {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                Experience Component 
+            </div>
+        );
+    }
+}
+
+export default Education;
