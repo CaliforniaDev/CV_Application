096f27500531b7cb0c885758755092b72a7c6c78
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-30 00:45:43 -0800

    Remove test content

--- cv-project/src/components/CVPreview/CVPreview.js
+++ cv-project/src/components/CVPreview/CVPreview.js
@@ -8,14 +8,9 @@ class CVPreview extends React.Component {
 
     render() {
       
-        const { firstName, lastName, email, phoneNumber } = this.props.personalInfo;
         return (
             <div className={styles.previewContainer}>
                 <section>
-                <p> {firstName} </p>
-                <p> {lastName} </p>
-                <p> {email} </p>
-                <p> {phoneNumber} </p>
                 </section>
             </div>
         )
