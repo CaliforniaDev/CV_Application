a30131c83d31d550d04ac09dfd50b7680be49afe
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-12-30 17:23:05 -0800

    Change component layout

--- cv-project/src/components/CVPreview/CVHeader.js
+++ cv-project/src/components/CVPreview/CVHeader.js
@@ -1,12 +1,14 @@
 import styles from '../../styles/CVPreview.module.scss';
 
 const CVHeader = ({ personalInfo }) => {
-    const { firstName, lastName } = personalInfo;
+    const { firstName, lastName, title } = personalInfo;
     return (
-        <header className={styles.headerWrapper}>
-            <h1>
-                {firstName} {lastName}
-            </h1>
+        <header className={styles.headerContainer}>
+            <div className={styles.headerWrapper}>
+                <h1>{firstName} {lastName}</h1>
+                <h3>{title}</h3>
+            </div>
+
         </header>
     )
 }
