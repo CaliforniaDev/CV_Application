80d6d669f93adcc56878582d88aec657ecfb0895
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-01-03 16:47:43 -0800

    Remove all previous content from cv body

--- cv-project/src/components/CVPreview/CVBody.js
+++ cv-project/src/components/CVPreview/CVBody.js
@@ -32,39 +32,7 @@ const CVBody = ({ personalInfo, experience, education, skills }) => {
 
     return (
         <div>
-            <section className={styles.personalInfoSection}>
-                <div className={styles.contactsDiv}>
-                    <div className={styles.contactsWrapper}>
-                        <h4>PHONE</h4>
-                        <p>{personalInfo.phoneNumber}</p>
-                        <h4>MAIL ADDRESS</h4>
-                        <p>{personalInfo.address}</p>
-                        <p>{personalInfo.city}, {personalInfo.state} {personalInfo.zipCode}</p>
-                        <h4>WEBSITE</h4>
-                        <p>{personalInfo.website}</p>
-                    </div>
-                </div>
-
-                <div className={styles.profileDiv}>
-                    <div className={styles.profileWrapper}>
-                        <h4>Profile</h4>
-                        <p>{personalInfo.description}</p>
-                    </div>
-                </div>
-            </section>
-            <section className={styles.experienceSection}>
-                <div className={styles.skills}>
-                    <h4>SKILLS</h4>
-                    {skillItems}
-                </div>
-                {experimentItems}
-            </section>
-            <section className={styles.educationSection}>
-                <div className={styles.educationWrapper}>
-                <h4>Education</h4>
-                {educationItem}
-                </div>
-            </section>
+            
         </div>
     );
 }
