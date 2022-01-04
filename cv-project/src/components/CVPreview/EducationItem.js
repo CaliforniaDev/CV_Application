d79646e4c4e15ffa2ae59dd88c7fa9c7d31e8528
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-01-03 16:32:43 -0800

    Pass props to component

--- cv-project/src/components/CVPreview/EducationItem.js
+++ cv-project/src/components/CVPreview/EducationItem.js
@@ -1,9 +1,20 @@
 import styles from '../../styles/CVPreview.module.scss';
 
 const EducationItem = ({ educationItem }) => {
+    const {
+        school,
+        city,
+        state,
+        degree,
+        subject,
+        from,
+        to
+    } = educationItem;
     return (
-        <div>
-            <p>{educationItem.school}</p>
+        <div className={styles.educationItemWrapper}>
+            <p>{school}</p>
+            <p>Degree: {degree}</p>
+            <p>Subject: {subject}</p>
         </div>
     );
 }
