aea22868bc4bf62b3631a738c26e2b7ca900e482
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-01-08 19:57:00 -0800

    Add structure to our skills component

--- cv-project/src/components/CVPreview/Skills.js
+++ cv-project/src/components/CVPreview/Skills.js
@@ -1,8 +1,28 @@
-const Skills = ({ skillItem }) => {
-	console.log(skillItem);
+import styled from "styled-components";
+import Section from "../Utils/Section";
+import SubSection from "../Utils/SubSection";
+
+const Skills = ({ skills }) => {
+
+	const skillItems = skills.map(skillItem => {
+		return (
+			<li key={skillItem.id}>{skillItem.skill}</li>
+		)
+	});
 	return (
-		<p>{skillItem.skill}</p>
-	)
+		<Section>
+			<SubSection title="Skills">
+			<ListWrapper>
+				{skillItems}
+			</ListWrapper>
+		</SubSection>
+		</Section>
+		
+
+	);
 }
+const ListWrapper = styled.ul`
+	list-style: none outside;
+`
 
 export default Skills;
