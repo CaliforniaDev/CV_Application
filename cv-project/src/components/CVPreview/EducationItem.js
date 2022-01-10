0bce44d619c6952326612452caccc32500d27341
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-01-10 01:38:37 -0800

    Style education item component

--- cv-project/src/components/CVPreview/EducationItem.js
+++ cv-project/src/components/CVPreview/EducationItem.js
@@ -1,3 +1,6 @@
+import styled from "styled-components";
+import  Section from "../Utils/Section";
+import SubSection from "../Utils/SubSection";
 const EducationItem = ({ educationItem }) => {
   const {
     school,
@@ -9,12 +12,23 @@ const EducationItem = ({ educationItem }) => {
     to
   } = educationItem;
   return (
-    <div>
-      <p>{school}</p>
-      <p>Degree: {degree}</p>
-      <p>Subject: {subject}</p>
-    </div>
+    <EducationWrapper>
+      <Section>
+        <SubSection title="Education">
+          <h5>{school}</h5>
+          <p><span>Degree: </span>{degree}</p>
+          <p><span>Subject: </span>{subject}</p>
+          <p>{from}-{to}</p>
+        </SubSection>
+      </Section>
+    </EducationWrapper>
   );
 }
+
+const EducationWrapper = styled.div`
+  span {
+    font-weight: bold;
+  }
+`
 
 export default EducationItem;
