f3ad56ddedfb696ea91489edb01f3a51573a0d2c
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-01-10 01:39:29 -0800

    Map education items onto the sidebar component

--- cv-project/src/components/CVPreview/Sidebar.js
+++ cv-project/src/components/CVPreview/Sidebar.js
@@ -1,22 +1,40 @@
 import ContactInfo from "./ContactInfo";
 import Skills from "./Skills";
+import Section from "../Utils/Section";
+import EducationItem from "./EducationItem";
 import styled from "styled-components";
 
-const Sidebar = ({ personalInfo, skills }) => {
+const Sidebar = ({ personalInfo, education, skills }) => {
+
+  const educationItems = education.map(educationItem => {
+    return (
+      <EducationItem 
+        key={educationItem.id}
+        educationItem={educationItem}
+      />
+    );
+  })
+
   return (
     <SidebarWrapper>
       <ContactInfo personalInfo={personalInfo} />
+      {educationItems}
       <Skills skills={skills} />
     </SidebarWrapper>
   );
 }
 
 const SidebarWrapper = styled.div`
+  padding: 2rem;
     h4 {
         text-transform: uppercase;
     }
+    white-space: nowrap;
     display: flex;
     flex-direction: column;
 `;
 
+const Photo = styled.img`
+
+`
 export default Sidebar;
