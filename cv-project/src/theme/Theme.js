9f9b5f9e7dadb088878e11c968a319ae06581e66
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-01-11 23:09:00 -0800

    Style pdf button

--- cv-project/src/theme/Theme.js
+++ cv-project/src/theme/Theme.js
@@ -7,6 +7,8 @@
         addHover: '#222',
         deleteButton: '#555',
         deleteHover: '#333',
+        pdfButton: '#107a33',
+        pdfHover: '#169440',
         loadButton: '#e36619',
         loadHover: '#d4590d',
         resetButton: '#cf2e2e',
